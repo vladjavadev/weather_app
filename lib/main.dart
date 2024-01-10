831bcb45b773ec119e2bc000ddde5a2982ba449b
vladjavadev <vharcuk02@gmail.com>
2024-01-10 15:21:19 +0200

    set common styles for buttons, text and background

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/ui/routes/route.dart';
 
 void main() {
@@ -8,6 +9,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(title: 'Weather', routerConfig: goRouter);
+    return MaterialApp.router(
+        title: 'Weather',
+        theme: ThemeData(
+          useMaterial3: true,
+          appBarTheme:
+              const AppBarTheme(color: const Color.fromARGB(255, 14, 79, 133)),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 85, 192, 187),
+              background: const Color.fromARGB(255, 148, 200, 243)),
+
+          textButtonTheme: const TextButtonThemeData(
+              style: ButtonStyle(
+            backgroundColor:
+                MaterialStatePropertyAll(Color.fromARGB(186, 32, 159, 194)),
+          )),
+
+          textTheme: TextTheme(
+            //set title style
+            titleLarge: GoogleFonts.oswald(
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+            ),
+            bodyMedium: GoogleFonts.merriweather(),
+          ),
+        ),
+        routerConfig: goRouter);
   }
 }
